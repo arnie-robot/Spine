5d1997a4ceedbe495a22e07da43b2ab34a8c501c
Chris <chris@chris-alexander.co.uk>
2011-02-24 17:42:56 +0000

    thresholding, optionally outputs can not be sent signals too large commenting improvements, was so messy

--- src/spine.py
+++ src/spine.py
@@ -8,6 +8,7 @@ import copy
 import signal
 import sys
 import logging
+import math
 
 # Custom imports
 import Thread
@@ -29,20 +30,55 @@ def main():
 
     logging.info("Initialising connections")
     inputData = {}
+    sentData = {}
     while (True):
+        # Store up the previous data we had
         previousData = copy.copy(inputData)
+
+        # Load up input data from all of the connections
         inputData = {}
         for connection in connections:
             if connection['input'] not in inputData.keys():
                 inputData[connection['input']] = i.read(connection['input'])
+
+        # Decide if we want to dispatch any data
         for connection in connections:
+
+            # Check that past and previous data exists
             if (connection['input'] in previousData.keys()) and \
                 (connection['input'] in inputData.keys()):
+
+                # Decide if our past and previous data is different
                 if (previousData[connection['input']] != inputData[connection['input']]):
 
+                    # We have decided we do want to dispatch
                     t = time.gmtime()
                     timestring = str(t[3]) + ":" + str(t[4]) + ":" + str(t[5])
-                    logging.debug(timestring + ": Dispatching: " + connection['input'] + " => " + connection['output'])
+
+                    # See if we need to do a threshold check, and if it is possible
+                    outputInfo = c.getOutputInfo(connection['output']) # load up data
+                    if "threshold" in outputInfo.keys() and connection['output'] in sentData.keys():
+                        # We need to do the check
+                        checkElementsCount = 3
+                        if "checkElementsCount" in outputInfo:
+                            checkElementsCount = outputInfo['checkElementsCount']
+
+                        # Do the calculation
+                        squared = 0
+                        for k in range(checkElementsCount):
+                            inputval = inputData[connection['input']][0][k]
+                            sentval = sentData[connection['output']][0][k]
+                            squared = squared + math.fabs(math.pow(inputval - sentval, 2))
+                        diff = math.sqrt(squared)
+
+                        if diff > outputInfo['threshold']:
+                            # We are over the limit, we need to die
+                            logging.debug(timestring + ": Aborted dispatch: " + connection['input'] + " => " + connection['output'] + ", " + str(diff) + " is greater than " + str(outputInfo['threshold']))
+                            break;
+
+                    # No check or check passed
+                    logging.debug(timestring + ": Dispatching: " + connection['input'] + " => " + connection['output'] + " - " + str(inputData[connection['input']]))
+                    sentData[connection['output']] = copy.copy(inputData[connection['input']]) # Log we sent this
                     o.send(Thread.Message(connection['output'], copy.copy(inputData[connection['input']]))) # copy value or it goes BAD
 
 # Signal handler
